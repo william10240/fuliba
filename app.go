cff4c898368bb61358fe58b0461ba788e62bdc08
william <william@william.com>
2020-12-24 12:52:50 +0000

    增加gomod,规范变量命名

--- app.go
+++ app.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"./goimghdr"
 	"bufio"
 	"bytes"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
-	"github.com/sirupsen/logrus"
+	"fuliimg_go/goimghdr"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -16,204 +14,207 @@ import (
 	"regexp"
 	"strings"
 	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/sirupsen/logrus"
 )
 
 var logger = logrus.New()
 
-// 定义当前目录
-var APP_PATH, _ = os.Getwd()
+// _appPath 定义当前目录
+var _appPath, _ = os.Getwd()
 
-// 定义 图片目录
-var IMG_PATH = path.Join(filepath.Dir(APP_PATH), "fuliimages2")
+// _imgPath 定义 图片目录
+var _imgPath = path.Join(filepath.Dir(_appPath), "fuliimages2")
 
 const url = "https://fuliba2020.net/category/flhz"
 
 var reg = regexp.MustCompile(`(.*?)福利汇总第(.*?)期`)
 
-type MyFormatter struct{}
+type myFormatter struct{}
 
-func (s *MyFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+func (s *myFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	timestamp := time.Now().Local().Format("0000/00/00 00:00:00")
 	msg := fmt.Sprintf("%s [%s] %s\n", timestamp, strings.ToUpper(entry.Level.String()), entry.Message)
 	return []byte(msg), nil
 }
 
 func init() {
-	APP_PATH = strings.Replace(APP_PATH, "\\", "/",-1)
-	IMG_PATH = strings.Replace(IMG_PATH, "\\", "/",-1)
+	_appPath = strings.Replace(_appPath, "\\", "/", -1)
+	_imgPath = strings.Replace(_imgPath, "\\", "/", -1)
 
 	//logger.SetFormatter(&logrus.JSONFormatter{})
-	logrus.SetFormatter(new(MyFormatter))
+	logrus.SetFormatter(new(myFormatter))
 	logger.SetOutput(os.Stdout)
 	logger.SetLevel(logrus.InfoLevel)
 
-	println("----------当前运行路径: " + APP_PATH + " ----------")
-	println("----------图片存储路径: " + IMG_PATH + " ----------")
+	println("----------当前运行路径: " + _appPath + " ----------")
+	println("----------图片存储路径: " + _imgPath + " ----------")
 }
 
 // 获取 列表页
-func get_list(list_index int) {
-	list_url := url
-	if list_index > 1 {
-		list_url = fmt.Sprintf("%s%s%d", url, "/page/", list_index)
+func getList(listIndex int) {
+	listURL := url
+	if listIndex > 1 {
+		listURL = fmt.Sprintf("%s%s%d", url, "/page/", listIndex)
 	}
 
-	res, err := _request(list_url)
+	res, err := _request(listURL)
 	if err != nil {
-		logger.Error("列表页请求失败:" + list_url)
+		logger.Error("列表页请求失败:" + listURL)
 		//logger.Error(err)
 		return
 	}
 	doc, err := goquery.NewDocumentFromResponse(res)
 	if err != nil {
-		logger.Error("列表页解析失败:" + list_url)
+		logger.Error("列表页解析失败:" + listURL)
 		//logger.Error(err)
 		return
 	}
-	logger.Info("列表页请求成功:" + list_url)
+	logger.Info("列表页请求成功:" + listURL)
 
 	els := doc.Find("h2 a")
 	els.Each(func(i int, el *goquery.Selection) {
 		// 准备 参数
-		content_title, _ := el.Attr("title")
-		page_url, _ := el.Attr("href")
+		contentTitle, _ := el.Attr("title")
+		pageURL, _ := el.Attr("href")
 		// 测试查看参数
-		logger.Debug("content_title:" + content_title)
+		logger.Debug("content_title:" + contentTitle)
 		// 开始调用 get_page
-		get_page(page_url, content_title)
+		getPage(pageURL, contentTitle)
 	})
 
 }
 
 // 获取 内容页
-func get_page(page_url, page_title string) {
+func getPage(pageURL, pageTitle string) {
 
-	res, err := _request(page_url)
+	res, err := _request(pageURL)
 	if err != nil {
-		logger.Error("--内容页请求失败:" + page_url)
+		logger.Error("--内容页请求失败:" + pageURL)
 		//logger.Error(err)
 		return
 	}
 	doc, err := goquery.NewDocumentFromResponse(res)
 	if err != nil {
-		logger.Error("--内容页解析失败:" + page_url)
+		logger.Error("--内容页解析失败:" + pageURL)
 		//logger.Error(err)
 		return
 	}
-	logger.Info("--内容页请求开始:" + page_url)
+	logger.Info("--内容页请求开始:" + pageURL)
 
 	els := doc.Find(".article-paging a")
 	els.Each(func(i int, el *goquery.Selection) {
 		// 准备 参数
-		content_url, _ := el.Attr("href")
-		content_text := el.Text()
+		contentURL, _ := el.Attr("href")
+		contentText := el.Text()
 		// 开始调用 get_page
-		get_content(content_url, page_title, content_text)
+		getContent(contentURL, pageTitle, contentText)
 
 	})
 }
 
 // 获取 内容 分页
-func get_content(content_url, content_title, content_index string) {
+func getContent(contentURL, contentTitle, contentIndex string) {
 
-	res, err := _request(content_url)
+	res, err := _request(contentURL)
 	if err != nil {
-		logger.Error("----详情页请求失败:" + content_index + ":" + content_title)
+		logger.Error("----详情页请求失败:" + contentIndex + ":" + contentTitle)
 		//logger.Error(err)
 		return
 	}
 	doc, err := goquery.NewDocumentFromResponse(res)
 	if err != nil {
-		logger.Error("----详情页解析失败:" + content_index + ":" + content_title)
+		logger.Error("----详情页解析失败:" + contentIndex + ":" + contentTitle)
 		//logger.Error(err)
 		return
 	}
-	logger.Info("----详情页请求成功:" + content_index + ":" + content_title)
+	logger.Info("----详情页请求成功:" + contentIndex + ":" + contentTitle)
 
-	tag := reg.FindStringSubmatch(content_title)
+	tag := reg.FindStringSubmatch(contentTitle)
 	els := doc.Find(".article-content img")
 	els.Each(func(i int, el *goquery.Selection) {
 		// 准备 参数
-		img_src, _ := el.Attr("src")
-		if img_src == "" {
-			logger.Error("----src为空:" + content_index + ":" + content_title)
+		imgSrc, _ := el.Attr("src")
+		if imgSrc == "" {
+			logger.Error("----src为空:" + contentIndex + ":" + contentTitle)
 			return
 		}
-		img_path := path.Join(IMG_PATH, tag[1], tag[2], content_index, path.Base(img_src))
+		imgPath := path.Join(_imgPath, tag[1], tag[2], contentIndex, path.Base(imgSrc))
 		// 开始调用 save_img
-		save_img(img_src, img_path)
+		saveImg(imgSrc, imgPath)
 	})
 
 }
 
 // 保存图片
-func save_img(img_src, img_path string) {
-	logger.Debug("------开始下载图片:" + img_src)
+func saveImg(imgSrc, imgPath string) {
+	logger.Debug("------开始下载图片:" + imgSrc)
 	// 检测文件已近下载过
-	if _, err := os.Stat(img_path); err == nil {
-		logger.Debug("--------图片已下载过:" + img_path)
+	if _, err := os.Stat(imgPath); err == nil {
+		logger.Debug("--------图片已下载过:" + imgPath)
 		return
 	}
 
-	if matches, _ := filepath.Glob(img_path + "*"); len(matches) > 0 {
-		logger.Debug("--------图片已下载过:" + img_path)
+	if matches, _ := filepath.Glob(imgPath + "*"); len(matches) > 0 {
+		logger.Debug("--------图片已下载过:" + imgPath)
 		return
 	}
 
-	img_folder := path.Dir(img_path)
+	imgFolder := path.Dir(imgPath)
 
 	// 判断目录是否存在
-	if _, err := os.Stat(img_folder); err != nil {
-		err := os.MkdirAll(img_folder, os.ModePerm)
+	if _, err := os.Stat(imgFolder); err != nil {
+		err := os.MkdirAll(imgFolder, os.ModePerm)
 		if err != nil {
-			logger.Error("--------文件夹创建失败:" + img_folder)
+			logger.Error("--------文件夹创建失败:" + imgFolder)
 			//logger.Error(err)
 			return
 		}
 	}
 
-	response, err := _request(img_src)
+	response, err := _request(imgSrc)
 	if err != nil {
-		logger.Error("--------图片请求失败:" + img_src)
+		logger.Error("--------图片请求失败:" + imgSrc)
 		//logger.Error(err)
 		return
 	}
 	robots, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		logger.Error("--------图片读取失败:" + img_src)
-		logger.Error("              地址:" + img_path)
+		logger.Error("--------图片读取失败:" + imgSrc)
+		logger.Error("              地址:" + imgPath)
 		return
 	}
 	defer response.Body.Close()
 
 	// 如果文件名没有后缀
-	if path.Ext(img_src) == "" {
+	if path.Ext(imgSrc) == "" {
 		ext, err := goimghdr.WhatFromReader(bytes.NewReader(robots))
 		if err != nil {
-			logger.Error("--------图片解析失败:" + img_src)
-			logger.Error("              地址:" + img_path)
+			logger.Error("--------图片解析失败:" + imgSrc)
+			logger.Error("              地址:" + imgPath)
 			return
 		}
 		if ext == "jpeg" {
 			ext = "jpg"
 		}
-		img_path = img_path + "." + ext
+		imgPath = imgPath + "." + ext
 	}
 
-	file, err := os.Create(img_path)
+	file, err := os.Create(imgPath)
 	if err != nil {
-		logger.Error("--------图片创建失败:" + img_src)
-		logger.Error("              地址:" + img_path)
+		logger.Error("--------图片创建失败:" + imgSrc)
+		logger.Error("              地址:" + imgPath)
 		return
 	}
 	_, err = io.Copy(file, bytes.NewReader(robots))
 	if err != nil {
-		logger.Error("--------图片保存失败:" + img_src)
-		logger.Error("              地址:" + img_path)
+		logger.Error("--------图片保存失败:" + imgSrc)
+		logger.Error("              地址:" + imgPath)
 		return
 	}
-	logger.Info("--------图片保存成功:" + img_src)
-	logger.Info("              地址:" + img_path)
+	logger.Info("--------图片保存成功:" + imgSrc)
+	logger.Info("              地址:" + imgPath)
 }
 
 // 请求二进制
@@ -242,7 +243,7 @@ func _request(url string) (*http.Response, error) {
 
 func main() {
 	for i := 1; i < 8; i++ {
-		get_list(i)
+		getList(i)
 	}
 	logger.Info("请求完成,一小时后重试")
 	time.Sleep(60 * 60 * time.Second)
@@ -252,11 +253,11 @@ func main() {
 func main1() {
 
 	imgPath := "d:\\111\\"
-	imgUrl := "https://tva1.sinaimg.cn/large/007asALTgy1gl8ljm8thpj30mj0zttio.jpg"
+	imgURL := "https://tva1.sinaimg.cn/large/007asALTgy1gl8ljm8thpj30mj0zttio.jpg"
 
-	fileName := path.Base(imgUrl)
+	fileName := path.Base(imgURL)
 
-	res, err := http.Get(imgUrl)
+	res, err := http.Get(imgURL)
 	if err != nil {
 		fmt.Println("A error occurred!")
 		return
@@ -279,5 +280,5 @@ func main1() {
 }
 
 func main2() {
-	save_img("https://tva1.sinaimg.cn/large/007qbgWbgy1geaz4l0mqpj30m80tm4ea.jpg", "d:/111/007qbgWbgy1geaz4l0mqpj30m80tm4ea.jpg")
+	saveImg("https://tva1.sinaimg.cn/large/007qbgWbgy1geaz4l0mqpj30m80tm4ea.jpg", "d:/111/007qbgWbgy1geaz4l0mqpj30m80tm4ea.jpg")
 }
---
+++ go.mod
@@ -0,0 +1,15 @@
+module fuliimg_go
+
+go 1.15
+
+require (
+	github.com/PuerkitoBio/goquery v1.6.0
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/sirupsen/logrus v1.7.0
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
+)
